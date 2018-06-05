11f09e08c9db4677058aefc95f22e0833d77e575
NP-okada <okada@nxpg.net>
2018-06-05 15:06:08 +0900

    jQueryをnpm管理およびwebpackバンドルから外す

--- src/assets/js/entry.js
+++ src/assets/js/entry.js
@@ -1,38 +1,38 @@
-import $ from 'jquery';
+;(($) => {
+	$(() => {
+		var $w = $(window);
+		var bp = 640;
+		var init = () => {
+			console.log('initialized');
+		};
 
-$(() => {
-	var $w = $(window);
-	var bp = 640;
-	var init = () => {
-		console.log('initialized');
-	};
+		$(init);
 
-	$(init);
+		// レスポンシブ対応用イベントトリガー
+		(() => {
+			var size_base;
 
-	// レスポンシブ対応用イベントトリガー
-	(() => {
-		var size_base;
+			$(() => {
+				size_base = (window.innerWidth <= bp) ? 'sp' : 'notSp';
 
-		$(() => {
-			size_base = (window.innerWidth <= bp) ? 'sp' : 'notSp';
-
-			if (size_base === 'sp') {
-				$w.trigger('_size.sp');
-			} else {
-				$w.trigger('_size.notSp');
-			}
-		});
-		$w.on('resize', () => {
-			var size = (window.innerWidth <= bp) ? 'sp' : 'notSp';
-
-			if (size !== size_base) {
-				if (size === 'sp') {
+				if (size_base === 'sp') {
 					$w.trigger('_size.sp');
 				} else {
 					$w.trigger('_size.notSp');
 				}
-				size_base = size;
-			}
-		});
-	})();
-});
+			});
+			$w.on('resize', () => {
+				var size = (window.innerWidth <= bp) ? 'sp' : 'notSp';
+
+				if (size !== size_base) {
+					if (size === 'sp') {
+						$w.trigger('_size.sp');
+					} else {
+						$w.trigger('_size.notSp');
+					}
+					size_base = size;
+				}
+			});
+		})();
+	});
+})(jQuery);
