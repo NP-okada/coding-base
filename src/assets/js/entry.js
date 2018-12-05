d7a7afbb7fcbc52534411612c531970b55d9e76b
NP-okada <okada@nxpg.net>
2018-12-05 18:18:03 +0900

    entry.js修正

--- src/assets/js/entry.js
+++ src/assets/js/entry.js
@@ -4,43 +4,41 @@ import {module} from './import/module.js';
 import {component} from './import/component.js';
 import {other} from './import/other.js';
 
-;(($) => {
+(($) => {
+	var init = () => {
+		common();
+		module();
+		component();
+		other();
+		console.log('initialized');
+	};
+
+	$(init);
+
+	// レスポンシブ対応用イベントトリガー
 	(() => {
-		var init = () => {
-			common();
-			module();
-			component();
-			other();
-			console.log('initialized');
-		};
+		var baseSize;
 
-		$(init);
+		$(() => {
+			baseSize = (window.innerWidth <= g.bp) ? 'sp' : 'notSp';
 
-		// レスポンシブ対応用イベントトリガー
-		(() => {
-			var size_base;
+			if (baseSize === 'sp') {
+				g.$w.trigger('_size.sp');
+			} else {
+				g.$w.trigger('_size.notSp');
+			}
+		});
+		g.$w.on('resize', () => {
+			var size = (window.innerWidth <= g.bp) ? 'sp' : 'notSp';
 
-			$(() => {
-				size_base = (window.innerWidth <= g.bp) ? 'sp' : 'notSp';
-
-				if (size_base === 'sp') {
+			if (size !== baseSize) {
+				if (size === 'sp') {
 					g.$w.trigger('_size.sp');
 				} else {
 					g.$w.trigger('_size.notSp');
 				}
-			});
-			g.$w.on('resize', () => {
-				var size = (window.innerWidth <= g.bp) ? 'sp' : 'notSp';
-
-				if (size !== size_base) {
-					if (size === 'sp') {
-						g.$w.trigger('_size.sp');
-					} else {
-						g.$w.trigger('_size.notSp');
-					}
-					size_base = size;
-				}
-			});
-		})();
+				baseSize = size;
+			}
+		});
 	})();
 })(jQuery);
